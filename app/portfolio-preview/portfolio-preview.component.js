b5000ccea01570a943ed6976211d1166eb3dac80
Kevin Perez <unknotted26@gmail.com>
2017-02-27 09:31:37 -0500

    Some changes to image manipulation modal view.

--- app/portfolio-preview/portfolio-preview.component.js
+++ app/portfolio-preview/portfolio-preview.component.js
@@ -7,6 +7,5 @@ angular
     controller: ['Project',
       function PortfolioPreviewController(Project) {
         this.projects = Project.query();
-        //[{"modal": 1}, {"modal": 2}, {"modal": 3}, {"modal": 4}, {"modal": 5}, {"modal": 6}];
     }]
   });
