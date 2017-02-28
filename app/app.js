cc018b3cf71569539bd44fffb9f1d5d5630bacb7
Kevin Perez <unknotted26@gmail.com>
2017-02-28 01:48:16 -0500

    Created hack to display users resized image request. Works.

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ angular.module('myApp', [
   'core',
   'myApp.home',
   'portfolioPreview',
-
   'projectPage',
-  'myApp.version'
+  'myApp.version',
+  'ui.bootstrap'
 ]);
--- app/project-pages/project.controller.js
+++ app/project-pages/project.controller.js
@@ -2,7 +2,36 @@
 
 angular
   .module('projectPage')
-  .controller('ProjectPageController', ['$scope',
-    function($scope) {
-      $scope.greeting = "Hello, this is my main project page.";
+  .controller('ProjectPageController', ['$scope', '$routeParams', '$resource',
+    function($scope, $routeParams, $resource) {
+      $scope.projectTitle = $routeParams.projectId;
+      $scope.wasSubmitted = false;
+      //$scope.multiplier = 1;
+      var testURL = 'https://ideallyconnected.me/cgi-bin/cs50/pset4/resize.sh?multiplier=:multiplier&subbtn=Submit';
+      var request = $resource(testURL,{
+          multiplier: 10
+        }, {
+          newimage: {
+            method: 'GET',
+            headers: {
+              'Content-type': 'text/html'
+            }
+          }
+        }
+      );
+
+      $scope.submit = function(userInput) {
+        $scope.wasSubmitted = true;
+        request.newimage({ multiplier: $scope.multiplier }).$promise.then(function(response){
+          $scope.result = response;
+          // gather HTML data from server response
+          $scope.strhtml = new String();
+          for(var k in response){
+            if(k[0] == '$') break;
+            $scope.strhtml += response[k];
+          }
+        });
+      };
+
+      $scope.testHtml = "<div><h3>Hello from synthetic html</h3></div>";
   }]);
