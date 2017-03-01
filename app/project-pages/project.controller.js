e94791874a2ba9d484f543a0090d98f11f089a7d
Kevin Perez <unknotted26@gmail.com>
2017-02-28 20:24:28 -0500

    Renamed and removed variables. Complying with ECMAScript 6.

--- app/project-pages/project.controller.js
+++ app/project-pages/project.controller.js
@@ -5,20 +5,23 @@ angular
   .controller('ProjectPageController', ['$scope', '$routeParams', '$resource', '$window', 'projectDataFactory',
     function($scope, $routeParams, $resource, $window, projectDataFactory) {
       $scope.wasSubmitted = false;
-      $scope.projectDetails = projectDataFactory({ isArray: false, method: 'GET' }).query({ projectId: $routeParams.projectId });
+      $scope.projectDetails = projectDataFactory({ isArray: false, method: 'GET' })
+                                .query({ projectId: $routeParams.projectId });
+
+      // Put programming languages from array into a comma separated string
       $scope.languages = function(){
-        let lstring = new String();
-        let lans = $scope.projectDetails["programmingLanguages"];
-        for(let lan of lans){
-          lstring += lan;
-          if(lan != lans[lans.length-1])
-            lstring += ", ";
+        let _lang = new String();
+        let _langs = $scope.projectDetails["programmingLanguages"];
+        for(let lan of _langs){
+          _lang += lan;
+          if(lan != _langs[_langs.length-1])
+            _lang += ", ";
         }
-        return lstring;
+        return _lang;
       };
 
-      var testURL = 'https://ideallyconnected.me/cgi-bin/cs50/pset4/resize.sh?multiplier=:multiplier&subbtn=Submit';
-      var request = $resource(testURL, null, {
+      var programURL = 'https://ideallyconnected.me/cgi-bin/cs50/pset4/resize.sh?multiplier=:multiplier&subbtn=Submit';
+      var request = $resource(programURL, null, {
           newimage: {
             method: 'GET',
             headers: {
@@ -28,19 +31,17 @@ angular
         }
       );
 
-      $scope.retrieved = false;
-      // Called after form submission
+      // Called during form submission
       $scope.submit = function() {
         $scope.wasSubmitted = true;
         request.newimage({ multiplier: $scope.multiplier }).$promise.then(function(response){
           $scope.result = response;
-          // gather HTML data from server response
+          // extract HTML data from server response
           $scope.htmlString = new String();
           for(var k in response){
             if(k[0] == '$') break;
             $scope.htmlString += response[k];
           }
-          $scope.retrieved = true;
         });
       };
 
