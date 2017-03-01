c89bc1e98c6d6f3ed0bb3d1db48ef3ba270199da
Kevin Perez <unknotted26@gmail.com>
2017-02-28 20:23:00 -0500

    comments added

--- app/core/project/project.service.js
+++ app/core/project/project.service.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*
+  This service is used to extract data from json files and store it into memory. projectDataFactory can
+  be used as a function itself because, as you can see, the factory is returning a function that takes an argument.
+ */
 angular
   .module('core.project')
   .factory('projectDataFactory', ['$resource',
